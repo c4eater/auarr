004d6e0978e8f3b936019c78c089cad42931f05b
Oleg Kostyuchenko <olegk@sdf.org>
2016-07-26 12:14:20 +0300

    implement basic scanning of Vorbis tags

--- arrange.pl
+++ arrange.pl
@@ -1,4 +1,35 @@
 #!/usr/bin/perl
+#
+# Just another Perl script that fixes audio tags and sorts audio files.
+#
+# TODO: Add a normal workflow description.
+#
+# A directory is considered a "scans directory" if it meets _all_ of the
+# following criteria:
+#
+#     - Has no directories inside, only files;
+#     - All the files inside are of graphical types (jpg, bmp, etc)
+#
+#
+# A directory is considered an "album directory" if it meets _all_ of the
+# following criteria:
+#
+#     - The directory has no child directories other than a scans directory
+#       (optional);
+#
+#     - The directory contains audio data in one of the following forms:
+#           - list of audio tracks of the same type (mp3, flac, ape, etc)
+#           - an unsplit audio file (flac, wav, wv, iso, etc) + cuesheet +
+#             logfile (optional)
+#
+#     - The directory has no files other than the audio files specified above
+#       and, optionally, the following ones:
+#           - text files (txt, log) - will be ignored during export;
+#           - playlists (m3u, wpl, etc) - will be ignored during export;
+#           - image files (jpg, bmp etc) - will be considered scans/booklets
+#             and moved to a separate folder (except for "front.jpg" and
+#             "cover.jpg") - these will be considered front images and left
+#             in place; will be renamed to cover.jpg for consistency though.
 
 use strict;
 use warnings;
@@ -7,7 +38,7 @@ use File::pushd;
 use File::Spec "abs2rel";
 use Getopt::Long;
 use Term::ANSIColor;
-#use Data::Dumper;
+use Data::Dumper;
 
 
 
@@ -42,73 +73,91 @@ sub _warn {
 
 
 
-# Accept a directory as an argument, and return the current working
-# directory in a relative form, treating the argument as a basedir.
+# Accept a directory name as an argument, and return the current working
+# directory name in a relative form, treating the argument as a basedir.
 sub rcwd {
     return File::Spec->abs2rel(cwd(), $ARGV[0]);
 }
 
 
 
-# Given a FLAC file name, return a hash storing this file's Vorbis tags.
-sub vorbis_get {
-    my $file = shift or die "Error: \"file\" not specified";
-
+# Given a FLAC file name, return a hashref storing this file's Vorbis tags.
+# If there is an error fetching these tags, return 0.
+sub fetch_vorbis_tags_file {
+    my $file = shift or die "Error: \"file\" argument not specified";
     my $command = `metaflac --list --block-type=VORBIS_COMMENT \"$file\"`;
 
-    open(my $fh, '<', \$command);
+    return 0 if $?;
+
+    open(my $command_ostream, '<', \$command);
 
     my %vorbis_tags = ();
 
-    while (<$fh>) {
+    while (<$command_ostream>) {
         chomp;
         (my $key, my $value) = $_ =~ /comment\[[[:alnum:]]+\]: (.*)=(.*)/;
         $vorbis_tags{$key} = $value if $key && $value;
     }
 
-    close $fh;
+    close $command_ostream;
 
     return \%vorbis_tags;
 }
 
 
-# A directory is considered a "scans directory" if it meets _all_ of the
-# following criteria:
-#
-#     - Has no directories inside, only files;
-#     - All the files inside are of graphical types (jpg, bmp, etc)
-#
-#
-# A directory is considered an "album directory" if it meets _all_ of the
-# following criteria:
-#
-#     - The directory name:
-#           - either contains a year and an album name (guessed from the tags)
-#           - or:
-#               - equals to "CD1", "CD2" etc.
-#               - the parent directory name contains a year and an album name
-#               - the parent directory contains only child dirs "CD1", "CD2" etc.
-#
-#     - The directory has no child directories other than a scans directory
-#       (optional);
-#
-#     - The directory contains audio data in one of the following forms:
-#           - list of audio tracks (mp3, flac, ape, etc)
-#           - an unsplit audio file (flac, wav, wv, iso, etc) + cuesheet +
-#             logfile (optional)
-#
-#     - The directory has no files other than the audio files specified above
-#       and, optionally, the following ones:
-#           - text files (txt, log) - will be ignored during export;
-#           - playlists (m3u, wpl, etc) - will be ignored during export;
-#           - image files (jpg, bmp etc) - will be considered scans/booklets
-#             and moved to a separate folder (except for "front.jpg" and
-#             "cover.jpg") - these will be considered front images and left
-#             in place; will be renamed to cover.jpg for consistency though.
-#
-#
-# A directory is considered an "artist directory" if it has only album directories
-# underneath and, optionally, audio files.
+
+# Collect Vorbis tags of the given list of FLAC files if these files form a
+# valid album, and return it in an arrayref form.
+# If not, issue an error message and return 0.
+sub fetch_vorbis_tags_fileset {
+    my $files = shift;
+    my $file0 = shift $files;
+    my $tagset0 = fetch_vorbis_tags_file($file0);
+    my $has_missing_tag = 0;
+
+    unless ($tagset0) {
+        _error sprintf("NO_VORBIS_TAGS  %s/%s\n", rcwd, $file0);
+        return 0;
+    }
+
+    my @tagsets = ($tagset0);
+
+    # Check tagset0 for empty tags.
+    map( { unless ($tagsets[0]->{$_}) {
+               _error sprintf("    MISSING_TAG_%-8s    %s/%s\n",
+                              $_, rcwd, $$files[0]);
+               $has_missing_tag = 1; }
+         }
+         ("TITLE", "ARTIST", "ALBUM", "DATE", "TRACKNUMBER"));
+
+    return 0 if $has_missing_tag;
+
+    foreach my $file (@{$files}) {
+        my $tagset = fetch_vorbis_tags_file($file);
+
+        unless ($tagset) {
+            _error sprintf("    NO_VORBIS_TAGS  %s/%s\n", rcwd, $file);
+            return 0;
+        }
+
+        # Check the remaining tagsets for empty and mismatching tags.
+        # Terminate on first error.
+        map( { unless ($tagset->{$_}) {
+                   _error sprintf("    MISSING_TAG_%-8s    %s/%s\n",
+                                  $_, rcwd, $file);
+                   $has_missing_tag = 1; }
+             }
+             ("TITLE", "ARTIST", "ALBUM", "DATE", "TRACKNUMBER"));
+
+        if ($has_missing_tag) {
+            return 0;
+        } else {
+            push(@tagsets, $tagset);
+        }
+    }
+
+    return \@tagsets;
+}
 
 
 
@@ -127,20 +176,26 @@ sub fetch_scans {
 
 
 
-# Collect albums from a directory in a recursive way.
+# Collect albums from a directory in a recursive way, copying audio files
+# and CD cover scans from album directories to the output directory.
 #
-# If an album is successfully fetched and copied, the function returns 1.
+# If the function argument is an album directory and the album is successfully
+# fetched and copied, the function returns 1.
 # (This also results in removing the source dir in case --remove-source
 # has been activated.)
 #
-# Otherwise, the function returns 0.
+# Otherwise, the function returns 0. This can happen in two cases:
+#   - The directory has no audio files and the function just applied itself
+#     recursively to the subdirectories, hoping to fetch albums there;
+#   - An error occurred, e.g. the directory has mixed contents (audio files
+#     plus non-scans subdirectories).
 sub fetch_albums {
     my ($files, $dirs) = @_;
 
     foreach my $i (0..@$dirs-1) {
-        if (funcall(\&fetch_scans, ${$dirs}[$i])) {
+        if (apply_fn_to_dir(\&fetch_scans, ${$dirs}[$i])) {
             my $scans_dir = ${$dirs}[$i];
-            delete ${$dirs}[$i];
+            splice(@$dirs, $i, 1);
             last;
         }
     }
@@ -151,7 +206,7 @@ sub fetch_albums {
     }
 
     if (@$dirs) {
-        map({ funcall(\&fetch_albums, $_) } @$dirs);
+        map({ apply_fn_to_dir(\&fetch_albums, $_) } @$dirs);
         return 0;
     }
 
@@ -201,14 +256,8 @@ sub fetch_albums {
         return 1;
     } elsif (!@mp3 && (@flac>1) && !@ape) {
         printf("FLAC            %s\n", rcwd);
-        printf("=== DEBUG ===\n%s\n",
-               join("\n\n", map({ my $flacdata = vorbis_get($_);
-                                  join("\n",
-                                       map({ sprintf("%s = %s",
-                                                     $_, $flacdata->{$_}) }
-                                           keys($flacdata))) }
-                                @flac)));
-        print "=== DEBUG END ===\n\n";
+
+        my $data = fetch_vorbis_tags_fileset(\@flac);
 
         return 1;
     } else {
@@ -220,7 +269,7 @@ sub fetch_albums {
 
 
 # Apply the given subroutine to files and subdirectories of the given directory.
-sub funcall {
+sub apply_fn_to_dir {
     my $fn = shift or die "Error: \"fn\" not specified";
     my $dir = shift or die "Error: \"dir\" not specified";
 
@@ -259,4 +308,4 @@ if (defined $opt_help) {
     exit;
 }
 
-funcall \&fetch_albums, $ARGV[0];
+apply_fn_to_dir \&fetch_albums, $ARGV[0];
